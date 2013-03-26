d55b1ab4766e4ed91c2b63e5ab533ffc40fa3659
Mark Funk <mfunk86@gmail.com>
2013-03-25 22:07:41 -0400

    Updating gamestate with Constructors, tablature

--- server/gamestate.js
+++ server/gamestate.js
@@ -81,19 +81,20 @@ var GameState = (function(){
 	self.questionTime = null
 	self.admins = []
 
-	self.GameQuestion = {
-		body : null,
-		bodyIndex : -1,
-		questionsIndex : -1,
+	var GameQuestion = function(body, bodyIndex, questionsIndex) {
+		this.body = body;
+		this.bodyIndex = bodyIndex;
+		this.questionsIndex = questionsIndex;
 	}
 
-	self.QuestionBody = {
-		input : null,
-		output : null,
-		textQuestions : [],
-		questionIndex:-1,
-		questionCount:-1,
-		duration:0
+	var QuestionBody = function(inputs, correctOutput, timePerQuestion){
+		this.input = inputs
+		this.output = correctOutput
+		this.duration = self.settings.timePerQuestion
+		this.textQuestions = [];
+		this.questionIndex = -1;
+		this.questionCount = -1;
+		return this;
 	}
 
 	//internal functions and logic
@@ -206,7 +207,7 @@ var GameState = (function(){
 			},
 			function(error){
 				if(error)
-				console.error("Error saving players into mongo")
+					console.error("Error saving players into mongo")
 			}
 		);
 
@@ -240,264 +241,258 @@ var GameState = (function(){
 		);
 	}
 
-    self._finishQuestion = function(){
-	var answerObj = {answer:self.currentQuestion.bodyIndex}
-	pls = self.players.filter(function(vl,idx,arr){return vl.active})
-	async.series([
-	    //async each the answer to each player
-	    function(callback){
+	self._finishQuestion = function(){
+		var answerObj = {answer:self.currentQuestion.bodyIndex};
+		pls = self.players.filter(function(vl,idx,arr){return vl.active});
+		async.series(
+			[
+				//async each the answer to each player
+				function(callback){
+					async.each(pls.concat(self.admins),
+						function(person,callback){
+							universalModule.questionOver(person,answerObj);
+							callback();
+						},
+						function(err){
+							if(err)
+								console.error("error sending finish question");
+							callback(null);
+						}
+					);
+				},
+				//update the leaderboard asyhnc
+				function(callback){
+					self._updateLocalLeaderboard(callback);
+				}
+			],
+			function(err,reults){
+				//finish off cleaning up and fixing statuses
+				self.currentQuestion = null;
+				self.questionRunning = false;
+				//If we're out of questions, this game is over
+				if(self.questionSet.length == 0)
+					self._gameOver(false);
+			}
+		);
+	}
+
+	self._startQuestion = function(){
+		console.log("starting question");
+		//If we're running a question, don't do anything
+		if(self.questionRunning)
+			return;
+
+		//If we're out of questions, ignore this
+		if(self.questionSet == null)
+			return;
+
+		self.currentQuestion = self.questionSet.pop()
+		if(self.currentQuestion == null)
+			return;
+
+		self.questionRunning = true;
+
+		pls = self.players.filter(function(vl,idx,arr){
+			if(vl.active){
+				vl.answered = false;
+				return true;
+			}
+			return false;
+		});
+
+		//distribute the question
 		async.each(pls.concat(self.admins),
-			   function(person,callback){
-			       universalModule.questionOver(person,answerObj)
-			       callback()},
-			   function(err){
-			       if(err)
-				   console.error("error sending finish question")
-			       callback(null)})
-		    },
-	    //update the leaderboard asyhnc
-	    function(callback){
-		self._updateLocalLeaderboard(callback)
-	    }],
-		     function(err,reults){
-			 //finish off cleaning up and fixing statuses
-			 self.currentQuestion = null
-			 self.questionRunning = false
-			 //If we're out of questions, this game is over
-			 if(self.questionSet.length == 0)
-			     self._gameOver(false)
-			 
-		     })
-    }
+			function(person,callback){
+				universalModule.questionStart(person,self.currentQuestion.body);
+				callback();
+			},
+			function(err){
+				if(err)
+					console.error("error sending the question");
+			}
+		);
 
-    self._startQuestion = function(){
-	console.log("starting question")
-	//If we're running a question, don't do anything
-	if(self.questionRunning)
-	    return
+		self.questionTime = new Date();
 
-
-	//If we're out of questions, ignore this
-	if(self.questionSet == null)
-	    return
-
-	self.currentQuestion = self.questionSet.pop()
-	if(self.currentQuestion == null)
-	    return;
-
-	self.questionRunning = true
-
-	pls = self.players.filter(function(vl,idx,arr){
-	    if(vl.active){
-		vl.answered = false
-		return true
-	    }
-	    return false
-	})
-
-	//distribute the question
-	async.each(pls.concat(self.admins),
-		   function(person,callback){
-		       universalModule.questionStart(person,self.currentQuestion.body)
-		       callback()
-		   },
-		   function(err){
-		       if(err)
-			   console.error("error sending the question")
-		   })
-	    
-
-	    self.questionTime = new Date()
-
-	//wait the appropriate amount of time
-	setTimeout(self._finishQuestion,self.settings.timePerQuestion * 1000)
-	
-	
-    }
-
-    self._addPlayer = function(player,callback){
-	for(var i = 0 ; i < self.players.length;++i){
-	    if(self.players[i].hash == player.hash){
-		self.players.splice(i,1)
-		break
-	    }
+		//wait the appropriate amount of time
+		setTimeout(self._finishQuestion, self.settings.timePerQuestion * 1000)
 	}
 
-	self.players.push(player);
-	schema.getTopScores(10,function(scores){
-	    leaderboard = scores.map(function(vl,idx,arr){
-		return {handle:vl.handle,score:vl.score}
-	    })
-	    universalModule.updateGlobalBoard(player,leaderboard);
-	});
-	async.each(self.admins,
-		   function(admin,cb){
-		       adminModule.playerConnected(admin,self._getExternalPlayers(player))
-		       cb(null)
-		   },
-		   function(error){
-		       if(error)
-			   console.error("error sending admin update")
-		       
-		       callback(true)
-		   })
-	    }
-    
-
-    self._playerRequests = function(hash){
-	var player = self._getPlayer(hash)
-	if(player != null){
-	    player.joining = true
-	    console.log("Player joined: "+player.handle)
-	}
-    }
-
-    self._startGame = function(callback){
-	if(self.gameRunning){
-	    callback(false)
-	    return
-	}
-	self.gameRunning = true
-	async.series([self._createQuestionSet,self._initializePlayers],
-		     function(err,obj){
-			 if(err)
-			     console.error("failed to start game")
-			 callback()
-		     })
-    }
-
-
-    //Loop through all the players and include them in the game
-    self._initializePlayers = function(callback){
-	async.each(self.players,
-		   function(player,callback){
-		       if(player.joining){
-			   player.joining = false
-			   player.active = true
-			   player.currentScore = 0
-			   
-		       }
-		       callback(null)
-		   },
-		   function(err){
-		       console.log("initialized players");
-		       if(err){
-			   console.error("Failed to start player game")
-			   callback('Error in initialize players')
-		       }
-		       callback()
-		   })
-	    }
-
-    //This generates a set of questions
-    self._createQuestionSet =  function(callback){
-
-	console.log("start generating questions");
-
-
-	//http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-	//In place fisher yates shuffle
-	var shuffle= function(arr){
-	    for(i = 0;i<arr.length;++i){
-		var j = Math.floor(i*Math.random(i+1))
-		var a = arr[i]
-		arr[i] = arr[j]
-		arr[j] = a
-	    }
-	}
-
-	//Get a random set of questions to be our correct answers
-	var getCorrectIndexes = function(){
-	    indexes = []
-	    for(i=0;i<self.questions.length;++i){
-		indexes.push(i)
-	    }
-	    shuffle(indexes)
-	    return indexes.splice(0,self.settings.questionsPerSet)
-	}
-
-	//Takes a correct index, generates an output
-	var generateQuestions = function(index, cb){
-    	    console.log("generating question %s", index)
-
-	    //temporary, we need to decide how many inputs/outputs
-	    inputs = []
-	    for (i = Math.max(Math.floor(Math.random()*11),2);i > 0; --i)
-		inputs.push(Math.floor(Math.random()*10+1))
-	    
-	    console.log("Inputs look like this:")
-	    console.log(inputs)
-
-	    //contexted needed for the VM call
-	    var context = vm.createContext({input:inputs})
-    	    var functionTemplate = "(function(input) { middle })";
-    	    var renderedFunction = eval(functionTemplate.replace("middle",self.questions[index].code));
-    	    var correctOutput = renderedFunction(inputs);
-
-	    //var correctOutput = vm.runInContext(questions[index].code,context)
-	    totalQuestions = self.questions.length
-	    body = Object.create(self.QuestionBody)
-	    body.input=inputs
-	    body.output=correctOutput
-	    body.duration=self.settings.timePerQuestion
-	    body.textQuestions = []
-	    tempArray = []
-	    for(i = 0;i<3;++i){
-		var j = Math.floor(Math.random()*totalQuestions)
-		var subRenderedFunction = eval(functionTemplate.replace("middle",self.questions[j].code));
-		//We can't repeat the question 
-		//we also can't have the outputs be ambiguous
-		if(j != index &&
-		   subRenderedFunction(inputs) != correctOutput &&
-		   tempArray.indexOf(j) < 0
-		  ){
-		    tempArray.push(j)
-		    body.textQuestions.push(self.questions[j].formatted)
-		    continue
-		}
-		i -=1
-	    }
-	    //we need to randomly insert the correct question into the body
-	    correctPosition = Math.floor(Math.random()*4)
-	    body.textQuestions.splice(correctPosition,0,self.questions[index].formatted)
-	    var gameQuestion = Object.create(self.GameQuestion)
-	    gameQuestion.body=body
-	    gameQuestion.bodyIndex=correctPosition
-	    gameQuestion.questionsIndex=index
-
-    	    console.log("done generating question %s", index)
-
-	    cb(null,gameQuestion)
-	    
-	}
-
-	console.log("still generating questions");
-
-	var indexes = getCorrectIndexes()
-	async.map(indexes,generateQuestions,function(err,newSet){
-	    console.log("generated questions nearly complete");
-	    if(err)
-		console.error("failed to generate questions")
-	    len = newSet.length
-	    for(i = 0;i<len;++i){
-		newSet[i].body.questionIndex = len-i-1
-		newSet[i].body.questionCount = len
-	    }
-
-	    self.questionSet = newSet
-
-	    console.log("generated questions");
-	    callback(null)
-	})
-    }
-
-	self._getPlayer = function(hash){
+	self._addPlayer = function(player,callback){
 		for(var i = 0 ; i < self.players.length;++i){
-			if(self.players[i].hash == hash){
-				return self.players[i]
+			if(self.players[i].hash == player.hash){
+				self.players.splice(i,1)
+				break
 			}
 		}
-		return null
+
+		self.players.push(player);
+		schema.getTopScores(10,function(scores){
+			leaderboard = scores.map(function(vl,idx,arr){
+				return {handle:vl.handle,score:vl.score}
+			});
+			universalModule.updateGlobalBoard(player,leaderboard);
+		});
+		async.each(self.admins,
+			function(admin,cb){
+				adminModule.playerConnected(admin,self._getExternalPlayers(player));
+				cb(null)
+			},
+			function(error){
+				if(error)
+					console.error("error sending admin update")
+				callback(true)
+			}
+		)
+	}
+
+	self._playerRequests = function(hash){
+		var player = self._getPlayer(hash);
+		if(player != null){
+			player.joining = true;
+			console.log("Player joined: " + player.handle);
+		}
+	}
+
+	self._startGame = function(callback){
+		if(self.gameRunning){
+			callback(false);
+			return;
+		}
+		self.gameRunning = true;
+
+		async.series(
+			[self._createQuestionSet,self._initializePlayers],
+			function(err,obj){
+				if(err)
+					console.error("failed to start game");
+				callback();
+			}
+		);
+	}
+
+
+	//Loop through all the players and include them in the game
+	self._initializePlayers = function(callback){
+		async.each(self.players,
+			function(player,callback){
+				if(player.joining){
+					player.joining = false;
+					player.active = true;
+					player.currentScore = 0;
+				}
+				callback(null);
+			},
+			function(err){
+				console.log("initialized players");
+				if(err){
+					console.error("Failed to start player game");
+					callback('Error in initialize players');
+				}
+				callback();
+			}
+		);
+	}
+
+	//This generates a set of questions
+	self._createQuestionSet =  function(callback){
+		console.log("start generating questions");
+
+		//http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+		//In place fisher yates shuffle
+		var shuffle= function(arr){
+			for(i = 0;i<arr.length;++i){
+				var j = Math.floor(i*Math.random(i+1));
+				var a = arr[i];
+				arr[i] = arr[j];
+				arr[j] = a;
+			}
+		}
+
+		//Get a random set of questions to be our correct answers
+		var getCorrectIndexes = function(){
+			indexes = [];
+			for(i=0;i<self.questions.length;++i){
+				indexes.push(i);
+			}
+			shuffle(indexes);
+			return indexes.splice(0,self.settings.questionsPerSet);
+		}
+
+		//Takes a correct index, generates an output
+		var generateQuestions = function(index, cb){
+			console.log("generating question %s", index)
+
+			//temporary, we need to decide how many inputs/outputs
+			inputs = [];
+			for (i = Math.max(Math.floor(Math.random()*11),2);i > 0; --i)
+				inputs.push(Math.floor(Math.random()*10+1));
+
+			console.log("Inputs look like this:");
+			console.log(inputs);
+
+			//contexted needed for the VM call
+			var context = vm.createContext({input:inputs});
+			var functionTemplate = "(function(input) { middle })";
+			var renderedFunction = eval(functionTemplate.replace("middle",self.questions[index].code));
+			var correctOutput = renderedFunction(inputs);
+
+			totalQuestions = self.questions.length;
+			body = new QuestionBody(inputs, correctOutput, self.settings.timePerQuestion);
+			tempArray = [];
+			for(i = 0;i<3;++i){
+				var j = Math.floor(Math.random() * totalQuestions);
+				var subRenderedFunction = eval(functionTemplate.replace("middle",self.questions[j].code));
+				//We can't repeat the question 
+				//we also can't have the outputs be ambiguous
+				if(j != index &&
+					subRenderedFunction(inputs) != correctOutput &&
+					tempArray.indexOf(j) < 0
+				){
+					tempArray.push(j)
+					body.textQuestions.push(self.questions[j].formatted)
+					continue
+				}
+				i -=1
+			}
+
+			//we need to randomly insert the correct question into the body
+			correctPosition = Math.floor(Math.random()*4)
+			body.textQuestions.splice(correctPosition,0,self.questions[index].formatted)
+			var gameQuestion = new GameQuestion(body, correctPosition, index);
+
+			console.log("done generating question %s", index)
+
+			cb(null, gameQuestion)
+		}
+
+		console.log("still generating questions");
+
+		var indexes = getCorrectIndexes();
+		async.map(indexes, generateQuestions, function(err,newSet){
+			console.log("generated questions nearly complete");
+			if(err)
+				console.error("failed to generate questions");
+			len = newSet.length;
+			for(i = 0;i<len;++i){
+				newSet[i].body.questionIndex = len-i-1;
+				newSet[i].body.questionCount = len;
+			}
+
+			self.questionSet = newSet;
+
+			console.log("generated questions");
+			callback(null);
+		});
+	}
+
+	self._getPlayer = function(hash){
+		for(var i = 0; i < self.players.length; ++i){
+			if(self.players[i].hash == hash){
+				return self.players[i];
+			}
+		}
+		return null;
 	}
 
 	self._playerAnswers = function(hash,answerIndex){
